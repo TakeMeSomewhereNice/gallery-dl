7dd7ffa4dfbfb3148e63046f6c61d8a64460d66f
Take Me Somewhere Nice <xlzsge@proton.me>
2026-06-22 02:15:06 -0700

    Tried adding pagination but failed. Needs to be fixed

--- gallery_dl/extractor/grajapa.py
+++ gallery_dl/extractor/grajapa.py
@@ -7,18 +7,51 @@
 """Extractors for https://www.grajapa.shueisha.co.jp/"""
 
 from .common import Extractor, Message
-from .. import text
+from .. import text, util
 
 class GrajapaExtractor(Extractor):
+    BASE_PATTERN  = r"(?:https?://)?(?:www\.)?grajapa\.shueisha\.co\.jp"
+    SPECIAL_PATTERN = BASE_PATTERN + (r"/plus/special/archives/(\d+)/chapter(\d)\.html"
+    SPECIAL_PATTERN = BASE_PATTERN + r"/plus/special/archives/\d+/contents/images/chapter(\d)/(\d+)-chapter(\d)-[\w.]+"
+    example = "https://www.grajapa.shueisha.co.jp/plus/special/archives/239/chapter1.html"
+    example = "https://www.grajapa.shueisha.co.jp/plus/special/archives/239/contents/images/chapter2/001-chapter2-hongo_yuzuha.jpg"
+
+def images(self, page):
+    """Return all image URLs from a paginated gallery"""
+    url = self.gallery_url
+    images = []
+    page_num = 1
+
+    while True:
+        self.log.info("Downloading page %d", page_num)
+        response = self.request(url)
+
+        # Extract images from current page
+        page_images = self._extract_images_from_page(response.text)
+        images.extend(page_images)
+
+        # Look for next page link
+        next_url = text.extract(response.text, 'class="chapter-pagination-item" href="chapter(\d).html"')[0]
+        if not next_url:
+            return images
+
+        url = self.root + next_url
+        page_num += 1
+
     """Extractor for grajapa specials"""
     category = "grajapa"
     subcategory = "special"
+    root = "https://www.grajapa.shueisha.co.jp/"
     directory_fmt = ("{category}", "{special}")
-    filename_fmt = "{tim} {filename}.{extension}"
+    filename_fmt = "{filename}.{extension}"
+    cookies_domain = ".grajapa.shueisha.co.jp"
 
-    pattern = (BASE_PATTERN +
-               r"(plus/special/archives/\d+/contents/images/chapter(\d)/(\d+)-chapter(\d)-[\w.]+)")
-    example = "https://www.grajapa.shueisha.co.jp/plus/special/archives/239/contents/images/chapter2/001-chapter2-hongo_yuzuha.jpg"
+    def __init__(self, match):
+        if match[1] == "grajapa":
+            self.category = "grajapa"
+            self.root = "https://www.grajapa.shueisha.co.jp/"
+            self.cookies_domain = ".grajapa.shueisha.co.jp"
+        Extractor.__init__(self, match)
 
     def items(self):
         yield Message.Version, 1
